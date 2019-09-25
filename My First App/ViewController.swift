ef5fa4e8a200c67a448082a4d252041861b330b3
Alina <alinabeglarian@gmail.com>
2019-09-25 17:57:39 +0200

    Initial Commit

---
+++ My First App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by Alina Beglarian on 25/09/2019.
+//  Copyright © 2019 Alina Beglarian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
